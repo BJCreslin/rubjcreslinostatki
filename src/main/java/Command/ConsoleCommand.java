e0dffd7512c42f44f5aaf2942ffa51bd291f125d
bjcreslin <bjcreslin@gmail.com>
2018-11-19 10:20:23 +0700

    added function

--- src/main/java/Command/ConsoleCommand.java
+++ src/main/java/Command/ConsoleCommand.java
@@ -1,6 +1,10 @@
 package Command;
 
 import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ConsoleCommand {
@@ -8,5 +12,11 @@ public class ConsoleCommand {
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
         return scanner.nextInt();
 
+    }
+
+    public static String dateOut() {
+        DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss", new Locale("ru"));
+        Date currentDate = new Date();
+        return dateFormat.format(currentDate);
     }
 }
